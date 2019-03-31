f8b6230382a9bf22078fa04706ae8955e4185b94
张易亮 <39492891+zylcs@users.noreply.github.com>
2019-03-31 13:58:27 +0800

    Update QQ_zone.py
    Add the recognition mechanism for Tencent sliding verification code

--- Crawler/QQ_zone.py
+++ Crawler/QQ_zone.py
@@ -15,6 +15,9 @@ import multiprocessing
 from multiprocessing import Pool,freeze_support
 import json
 import os
+import sys
+import recog_captcha
+from requests.adapters import HTTPAdapter
 class QQ_Spider(object):
 	def __init__(self,user,password):
 		'''
@@ -45,15 +48,28 @@ class QQ_Spider(object):
 		self.driver_zone.find_element_by_id('p').clear()
 		self.driver_zone.find_element_by_id('p').send_keys(self.__password)
 		self.driver_zone.find_element_by_id('login_button').click()
-		time.sleep(0.8) #等待浏览器加载
-		self.driver_zone.get('http://user.qzone.qq.com/{}'.format(self.__username))
-		#cookie = ''
+		goal_url='https://user.qzone.qq.com/{}'.format(self.__username)
+		time.sleep(0.8)
 		self.cookie_zone = {}
-		cookie_zone={}
+		cookie_zone = {}
+		count=0
+		while 1:
+			if self.driver_zone.current_url !=goal_url and count<=3:
+				time.sleep(1) #等待浏览器加载
+				count=count+1
+			elif self.driver_zone.current_url == goal_url and count>3 :
+				recog_captcha.recog_Verification_code(self.driver_zone)  #进行验证码识别
+				break
+			else:
+				break
+		self.driver_zone.get('https://user.qzone.qq.com/{}'.format(self.__username))
+		#cookie = ''
 		for item in self.driver_zone.get_cookies():
 			#cookie += item["name"] + '=' + item['value'] + ';'  #拼接cookie 为字符串
 			cookie_zone[item["name"]]=item['value']  #拼接cookie为字典
-
+		if not ('p_skey' in cookie_zone.keys()):
+			print("获取cookie值失败，请稍后再试...")
+			sys.exit(1)
 		self.cookie_zone = cookie_zone
 		# print(cookie_zone)
 		#self.headers['Cookie'] = self.cookies
@@ -71,6 +87,18 @@ class QQ_Spider(object):
 		self.driver_mail.find_element_by_id('p').send_keys(self.__password)
 		self.driver_mail.find_element_by_id('login_button').click()
 		time.sleep(0.8)
+		goal_url='https://mail.qq.com/cgi-bin/readtemplate?check=false&t=loginpage_new_jump&vt=passport&vm=wpt&ft=loginpage&target=&account={}'.format(
+				self.__username)
+		count=0
+		while 1:
+			if self.driver_mail.current_url !=goal_url and count<=3:
+				time.sleep(1) #等待浏览器加载
+				count=count+1
+			elif self.driver_mail.current_url == goal_url and count>3 :
+				recog_captcha.recog_Verification_code(self.driver_mail)  #进行验证码识别
+				break
+			else:
+				break
 		self.driver_mail.get(
 			'https://mail.qq.com/cgi-bin/readtemplate?check=false&t=loginpage_new_jump&vt=passport&vm=wpt&ft=loginpage&target=&account={}'.format(
 				self.__username))
@@ -105,6 +133,8 @@ class QQ_Spider(object):
 			, 'cookie': self.cookie_mail
 		}
 		session = requests.Session()
+		# session.mount('http://', HTTPAdapter(max_retries=3))
+		# session.mount('https://', HTTPAdapter(max_retries=3)) #设置超时重试次数
 		res = session.post(url=url, headers=headers, allow_redirects=False)
 		location = res.headers['location']
 		sid = location[location.find('sid=') + 4:location.find('&', location.find('sid='))]
@@ -130,8 +160,8 @@ class QQ_Spider(object):
 		# print(sortbyupdatetime)
 		sortbyupdatetime = eval(sortbyupdatetime)  # 将字符串转为list
 		# print(type(sortbyupdatetime))
-		for item in qq_groups:
-			print(str(int(item[0]) - 1000000) + ' : ' + item[2])  # 显示分组
+		for i,item in zip(range(len(qq_groups)),qq_groups) :
+			print(str(int(i)) + ' : ' + item[2])  # 显示分组信息 并打印
 		# for item in sortbyupdatetime:
 		# 	# print(item[0])
 		# 	if int(len(item[0])) < 10 or  re.findall('[a-zA-Z] ',item[0]):
@@ -141,27 +171,28 @@ class QQ_Spider(object):
 		count=0
 		for group in qq_groups:
 			count=count+len(group[1])
-		count = len(sortbyupdatetime)-count #除去因发送邮件而产生的无效信息
+		count = len(sortbyupdatetime)-count
 		# print(count)
 		for group in qq_groups:  #进行qq号转换
 			for num, i in zip(range(0, len(group[1])), range(count, count + len(group[1]))):
 				if str(group[1][num]) == sortbyupdatetime[i][0]:
 					group[1][num] = sortbyupdatetime[i][2]
+			# print(group[0])
 					# print(group[1][num])
 					# print(sortbyupdatetime[i][2])
 			count = count + len(group[1])
 		print("加载完成...")
 		print('请选择你要爬取的分组(输入对应分组的索引,可输入多个，以-1结束):')
-		while (1):
+		while (1):          #分组信息输入入口
 			i = int(input())
 			if i is not -1:
 				in_list.append(i)
 			else:
 				break
-		for i in in_list:
+		for i in in_list:    #将所选分组qq号填充到队列中
 			for num in qq_groups[i][1]:
 				qq_num.put(num)
-				print(num)
+				# print(num)
 		#print(self.qq_num)
 	def mode_choose(self,qq_num):
 		print("请选择爬取模式：1：指定QQ 2：指定分组 3：混合模式 ")
@@ -220,7 +251,6 @@ def get_info(g_tk,cookie_zone,qq_num):
 			t1, pos = True, 0
 			url_ = url + '&uin=' + str(q)
 			while(t1):
-
 				url__ = url_ + '&pos=' + str(pos)
 				info = req.get(url=url__, headers=headers,cookies=cookie_zone)
 				r = re.findall('\((.*)\)', info.text)[0]
@@ -252,12 +282,12 @@ def get_info(g_tk,cookie_zone,qq_num):
 							'comment': re.sub('cmtnum":', '', _comment),
 							'comment_content': re.sub('null|commentlist":', '', _comment_content) if 'null' in _comment_content else str([(re.sub('content":"|"', '', x), re.sub('createTime2":"|"', '', y), re.sub('name":"|"', '', z), re.sub('uin":', '', zz)) for x, y, z, zz in zip(re.findall('content":".*?"', _comment_content), re.findall('createTime2":".*?"', _comment_content), re.findall('name":".*?"', _comment_content), re.findall('uin":\d+', _comment_content))]),
 						}
-						with open('QQinfo.txt', 'a', encoding='utf-8') as f:
+						with open('QQinfo.txt', 'a', encoding='utf-8') as f:  #存储说说信息
 							f.write(str(data) + "\n")
 							f.close()
 							pid = os.getpid()  # 得到当前进程号
 							# print('当前进程号：%s   ' % pid)
-							# print("写入成功")
+							print("写入成功...")
 					pos += 20
 					time.sleep(random.uniform(4.0, 5.0))
 		else:
@@ -269,15 +299,16 @@ if __name__ == '__main__':
 	qq_num = manager.Queue()  # 初始化要爬取的qq列表,必须用multiprocess的manager下的queue 才能保持通信
 	user=input('请输入账号: ')
 	password=input('请输入密码: ')
+	process=int(input("请输入要开启的进程数: "))  #设定开启进程
 	qq_zone_crawler = QQ_Spider(user,password)
 	cookie_zone=qq_zone_crawler.QQ_zone_login()
 	g_tk=qq_zone_crawler.get_g_tk()
 	qq_zone_crawler.QQ_mail_login()
 	qq_zone_crawler.mode_choose(qq_num)
 	a = time.time()
-	p=Pool(4)
+	p=Pool(process)
 	results=[]
-	for i in range(4):
+	for i in range(process):
 		try:
 			results.append(p.apply_async(get_info, args=(g_tk,cookie_zone,qq_num)))
 		except  Exception as e:
